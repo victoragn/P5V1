3b38f70540053322fd89ce5a1b3758b4afc9ff99
victoragn <victor.agnez@hotmail.fr>
2018-02-11 17:35:43 +0100

    routes en comment / ajout d'un lien sur la page d'accueil vers la page de gestion de classes

--- src/Agnez/CoreBundle/Controller/DefaultController.php
+++ src/Agnez/CoreBundle/Controller/DefaultController.php
@@ -5,11 +5,13 @@ namespace Agnez\CoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
+class DefaultController extends Controller{
+     /**
+     * @Route("/", name="agnez_core_homepage")
+     */
+    public function indexAction(){
         $securityContext = $this->container->get('security.authorization_checker');
         if (!$securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
             return $this->redirectToRoute('fos_user_security_login');
@@ -17,5 +19,12 @@ class DefaultController extends Controller
             return $this->render('@AgnezCore/Default/index.html.twig');
         }
 
+    }
+
+     /**
+     * @Route("/classes", name="agnez_core_classes")
+     */
+    public function gestionClassesAction(){
+        return $this->render('@AgnezCore/Default/classes.html.twig');
     }
 }
