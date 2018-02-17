f7a1264b8fa260bc27bf922381376c9c96ad106c
victoragn <victor.agnez@hotmail.fr>
2018-02-17 21:17:01 +0100

    Ajout de formulaireType

--- src/Agnez/CoreBundle/Controller/DefaultController.php
+++ src/Agnez/CoreBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Agnez\CoreBundle\Form\ClasseType;
+use Agnez\CoreBundle\Form\FormulaireType;
 use Agnez\CoreBundle\Entity\Classe;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
@@ -45,25 +46,27 @@ class DefaultController extends Controller{
           ->getRepository('AgnezCoreBundle:Classe');
         $classes=$repository->findByUser( $this->getUser() );
 
-        $form = $this->createForm(FormType::class, $classes)
+        /*$form = $this->createForm(FormType::class, $classes)
             ->add('classes', CollectionType::class, array(
                 'entry_type' => ClasseType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
         ));
-        $form->add('save', SubmitType::class);
+        $form->add('save', SubmitType::class);*/
 
-        if ($request->isMethod('POST')) {
+
+        $form=$this->createForm(FormulaireType::class , $classes);
+        /*if ($request->isMethod('POST')) {
             $form->handleRequest($request);
 
             if ($form->isValid()) {
                 $em = $this->getDoctrine()->getManager();
-                $em->persist($classes);
+                $em->persist($classe);
                 $em->flush();
 
                 $request->getSession()->getFlashBag()->add('notice', 'Classe bien enregistrée.');
             }
-        }
+        }*/
         return $this->render('@AgnezCore/Classes/classes.html.twig', array(
             'form' => $form->createView(),
         ));
---
+++ src/Agnez/CoreBundle/Form/FormulaireType.php
@@ -0,0 +1,25 @@
+<?php
+namespace Agnez\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Agnez\CoreBundle\Form\ClassType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class FormulaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('classes',         CollectionType::class,array(
+                'entry_type'        => ClasseType::class,
+                'allow_add'         => true,
+                'by_reference'      => false
+            ))
+            ->add('ajouter la classe',      SubmitType::class)
+        ;
+    }
+}
